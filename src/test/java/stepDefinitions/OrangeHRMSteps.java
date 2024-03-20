5ee5284ca7085a4c1785bfd300a18e0cb848081c
mcrsr <111195628+mcrsr@users.noreply.github.com>
2024-03-20 07:01:23 +0530

    Update OrangeHRMSteps.java

--- src/test/java/stepDefinitions/OrangeHRMSteps.java
+++ src/test/java/stepDefinitions/OrangeHRMSteps.java
@@ -45,14 +45,14 @@ public class OrangeHRMSteps {
             System.out.println("Password: " + password);
             String logLevel = System.getenv("LOG_LEVEL");
             String tags = System.getenv("TAGS");
-            String username = System.getenv("USERNAME");
-            String password = System.getenv("PASSWORD");
+            String newusername = System.getenv("USERNAME");
+            String newpassword = System.getenv("PASSWORD");
             
             
         System.out.println("Log level: " + logLevel);
         System.out.println("Tags: " + tags);
-            System.out.println("username: " + username);
-            System.out.println("password: " + password);
+            System.out.println("new username: " + newusername);
+            System.out.println("new password: " + newpassword);
             // Close the input stream
             input.close();
         } catch (IOException e) {
